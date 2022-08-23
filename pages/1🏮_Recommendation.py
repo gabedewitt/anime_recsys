9468dc9b61eeefb42cbb4ab4bb3d75bd4e8f3a8f
Gabriel <gabrielvrsas@hotmail.com>
2022-08-23 19:16:48 -0300

    Adding wordcloud function Some updates to the code and wordcloud function added

--- pages/1🏮_Recommendation.py
+++ pages/1🏮_Recommendation.py
@@ -2,10 +2,23 @@ import streamlit as st
 import pandas as pd
 from skimage import io
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import itertools
 def show_Cover(url):
     a = io.imread(url)
     plt.imshow(a)
     plt.axis('off')
+    plt.show()
+
+def make_cloud(data, column):
+    list_column = data[column].tolist()
+    list_column = list(itertools.chain(*list_column))
+    strings = ' '.join(list_column)
+
+    plt.figure(figsize=(20,10))
+    wordcloud = WordCloud(max_words=100,background_color="white",width=800, height=400).generate(strings)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
     plt.show()
 
 def data_frame_demo():
@@ -23,20 +36,31 @@ def data_frame_demo():
 
     df = get_Anime_data()
     df_pred = preprocess(df)
+    df_pred.fillna(value = 'Not Found in MAL', inplace=True)
+
+    list_columns = ['genres','themes','demographics','studios'
+               ,'producers','licensors']
+
+    for column in list_columns:
+        df_pred[column] = df_pred[column].str.replace("['']","")
+        df_pred[column] = df_pred[column].str.replace("\[\]","")
+        
     anime_list = st.multiselect(
          "Choose some anime", list(df.title)
     )
+    st.dataframe(df.head())
+    st.dataframe(df_pred.head())
+    st.pyplot(make_cloud(df,'demographics'))
     if not anime_list:
         st.error("Please select an anime.")
     else:
         df_subset = df[df["title"].isin(anime_list)]
         for picture in df_subset.main_picture:
             st.write(f'Anime selected: {df.title[df.main_picture == picture].tolist()[0]}')
-            st.write(df[df.main_picture == picture].values.tolist())
+            st.dataframe(df_pred[df.main_picture == picture])
             st.image(picture, caption = picture)
             
-
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title="Recommendation", page_icon="🏮") 
 st.markdown("# Anime Suggestion")
 st.sidebar.header("Anime Suggestion")
